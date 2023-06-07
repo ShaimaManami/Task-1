1851a8f05b582ba5d4a7d02587b900faaef82f56
Shaima Al Sharif <103954813+ShaimaManami@users.noreply.github.com>
2023-06-07 09:42:33 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './main';
-import WeatherChart from './WeatherChart';
 import CityChart from './CityChart';
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/weather-chart" element={<WeatherChart />} />
         <Route path="/city-chart" element={<CityChart />} />
       </Routes>
     </BrowserRouter>
